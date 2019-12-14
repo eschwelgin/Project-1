be4f3d1196a926aacd603e25e2cb57bda84b151d
[eschwelgin] <[eschwelgin@gmail.com]>
2019-12-14 18:57:20 -0500

    working api call

--- README.md
+++ README.md
@@ -16,4 +16,4 @@ git commit -m "whatever"
 
 git push
 
-(test edit)
+
--- api-call.js
+++ api-call.js
@@ -1,30 +1,15 @@
-//https://sandbox-api.brewerydb.com/v2/
-//Key:b04f7e596969ab2a01ec840dc8a0d9c0
-<html>
-<head>
+// sandbox url https://sandbox-api.brewerydb.com/v2/
+// Key:b04f7e596969ab2a01ec840dc8a0d9c0
 
-
-</head>
-<body>
-<script>
-
-
-
-
-
-
+var styleId = 93 //range with current URL is 93-164
+  // have to use a cors proxy to prevent cross-origin resource sharing errors 
+  // https://cors-anywhere.herokuapp.com/
+var beerURL = 'https://cors-anywhere.herokuapp.com/sandbox-api.brewerydb.com/v2/beers?styleId=' + styleId + '&key=b04f7e596969ab2a01ec840dc8a0d9c0&withBreweries=Y'
 
 $.ajax({
-    url: "https://sandbox-api.brewerydb.com/v2/beer/O3tmVI/breweries/?key=abcdef",
-    method: "GET"
-  })
-    .then(function(response) {
-        console.log(response)
-    });
+    url: beerURL,
+    method: "GET",
+  }).then(function(response) {
+    console.log(response)
+});
 
-
-
-</script>
-
-</body>
-</html>
