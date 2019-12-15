52f5ddf365c6c23cd0d592cca2c9cfbc31cad24d
[eschwelgin] <[eschwelgin@gmail.com]>
2019-12-15 14:34:37 -0500

    added event listener

--- api-call.js
+++ api-call.js
@@ -3,17 +3,17 @@
 
 // have to use a cors proxy to prevent cross-origin errors 
 // https://cors-anywhere.herokuapp.com/
-var styleId = 93 //range with current URL is 93-164
+var styleId = 93 //range is 93-164
 var testbeerURL = 'https://cors-anywhere.herokuapp.com/sandbox-api.brewerydb.com/v2/beers?styleId=' + styleId + '&key=b04f7e596969ab2a01ec840dc8a0d9c0&withBreweries=Y'
 
 var userLat 
 var userLng 
-var testBrewURL = 'https://cors-anywhere.herokuapp.com/sandbox-api.brewerydb.com/v2/search/geo/point?lat=34.0522&lng=-118.2436&radius=100&key=b04f7e596969ab2a01ec840dc8a0d9c0' // LA, Cali // expected output =2 
+var testBrewURL = 'https://cors-anywhere.herokuapp.com/sandbox-api.brewerydb.com/v2/search/geo/point?lat=34.0522&lng=-118.2436&radius=100&key=b04f7e596969ab2a01ec840dc8a0d9c0' // LA, Cali
 
 function brewCall() {
   brewURL = 'https://cors-anywhere.herokuapp.com/sandbox-api.brewerydb.com/v2//search/geo/point?lat=' + userLat + '&lng=' + userLng + '&key=b04f7e596969ab2a01ec840dc8a0d9c0'
   $.ajax({
-    url: brewURL,
+    url: testBrewURL,
     method: "GET",
   }).then(function(response) {
     console.log(response)
@@ -33,4 +33,6 @@ function error(err) {
   console.log('ERROR(' + err.code + ":" + err.message + ')')
 }
 
-navigator.geolocation.getCurrentPosition(success, error)
+window.addEventListener("load", function () {
+  navigator.geolocation.getCurrentPosition(success, error)
+})
