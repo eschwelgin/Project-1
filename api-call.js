ab9f838b7f4e32e0bf51f0adcec136c54a62ab20
the137 <eschwelgin@gmail.com>
2019-12-17 22:04:52 -0500

    added images and next button

--- api-call.js
+++ api-call.js
@@ -1,19 +1,47 @@
 var userLat
 var userLng
+var brewName = ""
+var photoID = ""
+var i = 0
+
+mainImg = $("#mainImg")
+button = $("#btn")
+
+function setImgSrc(photoID) {
+  let mainImg = $('#mainImg')
+  let address = 'https://maps.googleapis.com/maps/api/place/photo?maxwidth=800&photoreference=' + photoID + "&key=AIzaSyCARpB8hXKo9eg9ffJNB4CZHM7pM3kTqrg"
+  mainImg.attr('src', address)
+}
+
+function brewGoog() {
+  // testGoogURL0 = 'https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=' + brewName + '&inputtype=textquery&fields=photo&key=AIzaSyCARpB8hXKo9eg9ffJNB4CZHM7pM3kTqrg'
+  googURL0 = 'https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=' + brewName + '&inputtype=textquery&fields=photo&key=AIzaSyCARpB8hXKo9eg9ffJNB4CZHM7pM3kTqrg'
+  // testGoogURL1 = "https://maps.googleapis.com/maps/api/place/photo?maxwidth=800&photoreference=CmRaAAAAw1AiqtnSdvjaSrmcwulcoSXtFEr3o5TQZMP2nVJirGyqmh1hHbldHX0LiVotdlymEgyAmPkOtofeNGuKqutn3TBkXkHNHwnoQgjsS7n2OtauzgP5o1CB8I6M2chvORFwEhBSObZmoQa0Nex_oKeW6xsTGhTn9WCDOlF4trANe9yaOwDCCGElWA&key=AIzaSyCARpB8hXKo9eg9ffJNB4CZHM7pM3kTqrg"
+
+  $.ajax({
+    url: googURL0,
+    method: "GET",
+  }).then(function(response) {
+    console.log(response)
+    photoID = response.candidates[0].photos[0].photo_reference
+    console.log(photoID)
+    setImgSrc(photoID)
+  });
+
+}
 
 function brewCall() {
-  // brewURL = 'https://cors-anywhere.herokuapp.com/sandbox-api.brewerydb.com/v2/search/geo/point?lat=' + userLat + '&lng=' + userLng + '&radius=100&key=b04f7e596969ab2a01ec840dc8a0d9c0'
-  //sub the test variable in the ajax call to get data back - I don't think the sandbox includes any cleveland breweries 
-  // test1 = 'https://cors-anywhere.herokuapp.com/sandbox-api.brewerydb.com/v2/breweries?&key=b04f7e596969ab2a01ec840dc8a0d9c0'
-  // test2 = "https://cors-anywhere.herokuapp.com/sandbox-api.brewerydb.com/v2/search/geo/point?lat=35.772096&lng=-78.638614&key=b04f7e596969ab2a01ec840dc8a0d9c0"
-
   brewURL = "https://api.openbrewerydb.org/breweries?by_city=cleveland"
-  
+
   $.ajax({
     url: brewURL,
     method: "GET",
   }).then(function(response) {
     console.log(response)
+    brewName = response[i].name
+    console.log(brewName)
+    brewGoog()
+
   });
 }
 
@@ -33,3 +61,11 @@ function error(err) {
 window.addEventListener("load", function () {
   navigator.geolocation.getCurrentPosition(success, error)
 })
+
+button.on("click", function() {
+  i++
+  brewCall()
+})
+
+//https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=Hofbrauhaus%20Cleveland&inputtype=textquery&fields=photo&key=AIzaSyCARpB8hXKo9eg9ffJNB4CZHM7pM3kTqrg
+//https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=CmRaAAAAw1AiqtnSdvjaSrmcwulcoSXtFEr3o5TQZMP2nVJirGyqmh1hHbldHX0LiVotdlymEgyAmPkOtofeNGuKqutn3TBkXkHNHwnoQgjsS7n2OtauzgP5o1CB8I6M2chvORFwEhBSObZmoQa0Nex_oKeW6xsTGhTn9WCDOlF4trANe9yaOwDCCGElWA&key=AIzaSyCARpB8hXKo9eg9ffJNB4CZHM7pM3kTqrg
