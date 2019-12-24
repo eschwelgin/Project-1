66c36d5013934fb309eeabf855d736fb7c068f02
the137 <eschwelgin@gmail.com>
2019-12-23 19:34:32 -0500

    added functionality

--- script2.js
+++ script2.js
@@ -1,38 +1,43 @@
-const userCity = document.querySelector('#search-city').value
-const userState = document.querySelector('#search-state').value
+let userCity = document.querySelector('#search-city').value
+let userState = document.querySelector('#search-state').value
+let results
 
 function searchBrewery () {
 
     
-    fetch(`https://api.openbrewerydb.org/breweries?by_city=${userCity}&by_state=${userState}`).then(result => {
+    fetch(`https://api.openbrewerydb.org/breweries?by_city=${userCity}&by_state=${userState}`)
+    .then(result => {
         return result.json()
     }).then(result => {
+    	results = result
         breweryInit(result)
     })
 }
-
 function breweryInit (brewFromServer) {
-    const results = brewFromServer
-
+    // results = brewFromServer
+    // let i = 0 
     
     const cardTitle = document.querySelector('#card-title')
     const cardText = document.querySelector('#card-text')
     const cardText2 = document.querySelector('#card-text2')
     const cardText3 = document.querySelector('#card-text3')
 
-    cardTitle.textContent = results[0].name
-    // cardText.textContent = results[0].street
-    // cardText2.textContent = results[0].city
-    // cardText3.textContent = results[0].state
+    cardTitle.textContent = results[i].name
+    cardText.textContent = results[i].street
+    cardText2.textContent = results[i].city
+    cardText3.textContent = results[i].state
     console.log(results)
+  
+}
 
-    const noBtn = document.querySelector('#noBtn')
-
-    noBtn.addEventListener('click', function() {
-    
-       
-    
+const noBtn = document.querySelector('#noBtn')
+let i=0
+noBtn.addEventListener('click', function() {
+i++
+breweryInit()
+console.log("click")
 })
+
 
 const searchBtn = document.querySelector('#search-btn')
 
@@ -43,8 +48,8 @@ const searchBtn = document.querySelector('#search-btn')
 // }
 searchBtn.addEventListener('click', function () {
     // console.log(userCity)
-    // userCity = document.querySelector('#search-city').value
-    // userState = document.querySelector('#search-state').value
+    userCity = document.querySelector('#search-city').value
+    userState = document.querySelector('#search-state').value
     searchBrewery()
 
     
